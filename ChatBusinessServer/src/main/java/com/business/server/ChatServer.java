97865abc846ab61d455f01de6164bd3bd227d3ca
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-05-18 21:49:36 -0500

    implemetacion de los casos en el servidor

--- ChatBusinessServer/src/main/java/com/business/server/ChatServer.java
+++ ChatBusinessServer/src/main/java/com/business/server/ChatServer.java
@@ -44,4 +44,62 @@ public class ChatServer {
             e.printStackTrace();
         }
     }
+
+    public void handleConnectionRequest(String username, String code){
+
+
+        switch (code){
+            case "01":
+                establishConnection(username);
+                break;
+            case "02":
+                confirmConnection(username);
+                break;
+            case "03":
+                sendUserList(username);
+                break;
+            case "04":
+                requestUserList(username);
+                break;
+            case "05":
+                sendMessage(username);
+                break;
+            case "06":
+                sendFile(username);
+                break;
+            case "07":
+                closeSession(username);
+                break;
+            default:
+                System.out.println("Código desconocido");
+                break;
+        }
+    }
+    private void establishConnection(String username) {
+    }
+
+    private void confirmConnection(String username) {
+    }
+
+    private void sendUserList(String username) {
+        // Lógica para enviar la lista de usuarios conectados
+    }
+
+    private void requestUserList(String username) {
+        // Lógica para solicitar la lista de usuarios conectados
+    }
+
+    private void sendMessage(String username) {
+        // Aquí implementa la lógica para enviar un mensaje
+    }
+
+    private void sendFile(String username) {
+        // Aquí implementa la lógica para enviar un archivo
+    }
+
+    private void closeSession(String username) {
+        // Lógica para cerrar la sesión del usuario
+    }
+
+
 }
